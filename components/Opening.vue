dc8af74e59a022a50f2963f4be3b5191f3324d46
Muliady1 <satuwilayahabadi@gmail.com>
2026-03-11 23:23:36 +0700

    wedding

--- components/Opening.vue
+++ components/Opening.vue
@@ -73,13 +73,24 @@ const themeClasses = computed(() => {
 })
 </script>
 <template>
+  <main>
   <AnimatePresence>
-    <div class="absolute inset-0">
+    <Motion
+      v-if="!isOpened"
+      key="overlay"
+      :initial="{ opacity: 1 }"
+      :exit="{ opacity: 0, y: -1000 }"
+      :transition="{ duration: 1, ease: 'easeInOut' }"
+      class="fixed inset-0 z-50 flex flex-col text-white text-center px-4 overflow-hidden">
+      <div class="absolute inset-0">
       <Nuxtimg src="https://images.unsplash.com/photo-1519741497674-611481863552?w=1920&q=80" alt="Couple"
         class="w-full h-full object-cover brightness-[0.4]" />
     </div>
     <div class="absolute inset-0 bg-gradient-to-t from-slate-900 via-slate-900/60 to-slate-900/40">
 
     </div>
+  </Motion>
+    
   </AnimatePresence>
+</main>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 
 <template>
     <slot />
+    <Navbar/>
     <Footer />
 </template>
